e4a5d737f1e575f90ed684b16a84adb31a8d8e44
David Betz <hashref@gmail.com>
2022-09-23 17:08:08 -0700

    Minor performance modification.
    Signed-off-by: David Betz <hashref@gmail.com>

--- converter.py
+++ converter.py
@@ -12,12 +12,12 @@ class NATO:
         self.translation_key = {row.letter: row.code for (index, row) in df.iterrows()}
 
     def convert(self, data: str) -> list[list[str]]:
-        parsed_data = re.sub(" +", " ", data).strip()  # Clean up text
+        parsed_data = re.sub(" +", " ", data).strip().upper()  # Clean up text
 
         converted_data = []
 
         for word in parsed_data.split(" "):
-            letters = [letter.upper() for letter in word]
+            letters = [letter for letter in word]
             converted_data.append(
                 [
                     self.translation_key[letter]
